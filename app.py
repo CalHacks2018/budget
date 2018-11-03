1cc1227814d5e5084fc5fd0dd61161b5d8f1584e
ndellamaria <nrdell3@gmail.com>
2018-11-02 21:01:24 -0700

    I can't spell

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return flask.render_tamplate("index.html")
+	return flask.render_template("index.html")
 
 if __name__=='__main__':
 	app.debug=True
