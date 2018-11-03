31480d859226ba92eb447d53fc270808d6abebff
ndellamaria <nrdell3@gmail.com>
2018-11-02 23:58:04 -0700

    added webhook stuff

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
 import flask 
 from flask import Flask, request, render_template 
 import requests
+import paypalrestsdk 
+from paypalrestsdk import Webhook
+
+paypalrestsdk.configure({
+	'mode': 'sandbox', #sandbox or live
+  	'client_id': 'ATbI73Ar9lZ6oeQEcjh-KPV9Zbe1q_x8k3A_CoV7liqZgI2lknrW5ZawTZnDhDRwc5L1eQNi0d8NHnCg',
+  	'client_secret': 'EM764Y5eYv8WM1dVs-z8lgqN2AQcmq_13BjuHVWhHr80bHxSLtVqBx7h9WWCHE8tIFa0_89DwG9kfWZ5' 
+})
+
+webhook = Webhook({
+	"url": "https://budget-track.herokuapp.com/",
+	"event_types": [{
+	"name": "PAYMENT.SALE.CREATED"
+	},{
+	"name": "PAYMENT.SALE.DENIED"
+ 	},{
+ 	"name": "PAYMENT.AUTHORIZATION.CREATED"
+ 	},{
+ 	"name": "PAYMENT.AUTHORIZATION.VOIDED"
+ 	}]
+ })
+
+if webhook.create():
+	print("Webhook[%s] created successfully" % (webhook.id))
+else:
+	print(webhook.error)
 
 app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	if request.method == 'POST':
+		result = request.json()
+		print(result)
+	else:
+		abort(400)
+
 
 @app.route('/')
 def main():
