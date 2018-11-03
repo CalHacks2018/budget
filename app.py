5f94caabf7d6eeb193925b88ef121ba3f50209ac
ndellamaria <nrdell3@gmail.com>
2018-11-02 20:56:02 -0700

    added code

---
+++ app.py
@@ -0,0 +1,14 @@
+import flask 
+from flask import Flask, request, render_template 
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def main();
+	return flask.render_tamplate("index.html")
+
+if __name__=='__main__':
+	app.debug=True
+	app.run()
+
