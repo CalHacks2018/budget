f42c90905b4dba468ad0f8eb298fe7036fc6afa7
ndellamaria <nrdell3@gmail.com>
2018-11-03 01:01:25 -0700

    added webhook stuff

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def webhook():
 	if request.method == 'POST':
 		req = request.get_json(silent=True, force=True)
 		print("Request:")
-		print(json.dumps(req, indent=4))
+		print(req)
 	else:
 		abort(400)
 
