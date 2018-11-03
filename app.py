4b63a1629474772d320063363597b75bfef654bb
ndellamaria <nrdell3@gmail.com>
2018-11-03 15:38:57 -0700

    he

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import Flask, request, render_template, redirect, jsonify, abort, url
 import requests
 from firebase_admin import db, initialize_app
 
+export GOOGLE_APPLICATION_CREDENTIALS=budget-data-d6bdc-firebase-adminsdk-bu5t8-383b28eb2d.json
 
 paypalrestsdk.configure({
 	'mode': 'sandbox', #sandbox or live
