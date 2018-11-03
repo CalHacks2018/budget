90407bcf84491b3fc0783fe75cc660208c9eb09d
Matthew <mdong@berkeley.edu>
2018-11-03 10:41:39 -0700

    firebase backend setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # budget
+
+```
+export GOOGLE_APPLICATION_CREDENTIALS=../budget-data-d6bdc-firebase-adminsdk-bu5t8-383b28eb2d.json
+export FLASK_APP=app.py
+flask run
+```
+
+## Backend API Doc
+
+curl -v -X POST -d @spiderman.json -H "Content-type: application/json" http://localhost:5000/heroes
+
+curl -X DELETE http://localhost:5000/heroes/-LQNfxvlYuLEj8QIGU3A
--- app.py
+++ app.py
@@ -1,14 +1,65 @@
 import flask 
 from flask import Flask, request, render_template 
 import requests
+import firebase_admin
+from firebase_admin import db
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+firebase_admin.initialize_app(options={
+    'databaseURL': 'https://budget-data-d6bdc.firebaseio.com'
+})
+USERS = db.reference('superheroes')
+
+@app.route('/login', methods =['POST'])
+def create_hero():
+    # print('[INFO]: ', request.form)
+    print('[INFO]: ', request.form['name'])
+    print('[INFO]: ', request.form['budget'])
+    # print('[INFO]: ', request.form.to_dict())
+    req = request.form.to_dict() # flask.request.json
+    hero = USERS.push(req)
+    return flask.jsonify({'id': hero.key}), 201 
+
+@app.route('/heroes', methods=['POST'])
+# def create_hero():
+#     # print('[INFO]: ', request.form)
+#     print('[INFO]: ', request.form['amount'])
+#     print('[INFO]: ', request.form['type'])
+#     print('[INFO]: ', request.form.to_dict())
+#     req = request.form.to_dict() # flask.request.json
+#     hero = USERS.push(req)
+#     return flask.jsonify({'id': hero.key}), 201
+
+@app.route('/heroes/<id>')
+def read_hero(id):
+    return flask.jsonify(_ensure_hero(id))
+
+@app.route('/heroes/<id>', methods=['PUT'])
+def update_hero(id):
+    _ensure_hero(id)
+    req = flask.request.json
+
+
+    USERS.child(id).update(req)
+    return flask.jsonify({'success': True})
+
+@app.route('/heroes/<id>', methods=['DELETE'])
+def delete_hero(id):
+    _ensure_hero(id)
+    USERS.child(id).delete()
+    return flask.jsonify({'success': True})
+
+def _ensure_hero(id):
+    hero = USERS.child(id).get()
+    if not hero:
+        flask.abort(404)
+    return hero
 
 @app.route('/')
 def main():
-	return flask.render_template("index.html")
+    return flask.render_template("login.html")
 
 if __name__=='__main__':
-	app.debug=True
-	app.run()
-
+    app.debug=True
+    app.run()
